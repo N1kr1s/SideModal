185fa8ce3e4175ad0e0d6de6610ed6834fd64dd2
Nio <nikris@mail.ru>
2024-07-04 17:31:11 +0300

    Added some functionality

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import { FaBars } from 'react-icons/fa'
+
+function Home() {
+  return (
+    <main>
+      <button className='sidebar-toggle'>
+        <FaBars />
+      </button>
+      <button className='btn'>show modal</button>
+    </main>
+  )
+}
+
+export default Home
